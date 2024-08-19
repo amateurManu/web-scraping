4128417ab5d98dd03b47ea196d507aa949959755
Emmanuel Odero <oderoemmanuel6@gmail.com>
2024-08-19 14:24:12 +0300

    added code that filters information needed from an html tag

---
+++ web-scraping_intro/main-1.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+""" Open file and read contents of file """
+with open('100-index.html', 'r') as html_file:
+    content = html_file.read()
+    
+    """ Creating an instance of BeautifulSoup """
+    soup = BeautifulSoup(content, 'lxml')
+
+    availabilities = soup.find_all('article')
+    for available in availabilities:
+        places = available.h2.text
+        amount = available.div.text
+        print(f"{places} costs {amount}")
